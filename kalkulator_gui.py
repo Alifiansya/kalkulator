e3f1c46ac6299d54aaeff7df5e69bde9d617dfb5
Alifiansya <alifiansyahfirdaus@gmail.com>
2022-09-27 17:57:03 +0700

    Added operator

--- kalkulator_gui.py
+++ kalkulator_gui.py
@@ -14,9 +14,38 @@ frame.pack()
 
 expr_field = Entry(
     frame,
-    width=24
+    width=33
 )
-expr_field.grid(columnspan=3, row=0)
+expr_field.grid(columnspan=4, row=0)
+
+op_btn_sym = ['C', '<=', '%', '/', '*', '-', '+']
+op_btn = dict()
+
+for i in range(3):
+    button = Button(
+        frame,
+        text=f"  {op_btn_sym[0]}  ",
+        fg='black',
+        bg='#D0B8A8',
+        command=None,
+        width=5
+    )
+    button.grid(row=1, column=i)
+    op_btn.update({op_btn_sym[0]: button})
+    op_btn_sym.pop(0)
+
+for i in range(4):
+    button = Button(
+        frame,
+        text=f"  {op_btn_sym[0]}  ",
+        fg='black',
+        bg='#D0B8A8',
+        command=None,
+        width=5
+    )
+    button.grid(row=i+1, column=3)
+    op_btn.update({op_btn_sym[0]: button})
+    op_btn_sym.pop(0)
 
 
 num_btn = list()
@@ -29,7 +58,7 @@ for i in range(1, 10):
         command=None,
         width=5
     )
-    button.grid(row=1 + (i-1) // 3, column=(i-1) % 3)
+    button.grid(row=2 + (i-1) // 3, column=(i-1) % 3)
     num_btn.append(button)
 
 
