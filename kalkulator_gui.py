4fa03a6986fcc14deb976683065a287e22da3b15
Alifiansya <alifiansyahfirdaus@gmail.com>
2022-09-27 17:35:12 +0700

    Added expression field

--- kalkulator_gui.py
+++ kalkulator_gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from turtle import color
 #color palette = https://colorhunt.co/palette/f8ede3dfd3c3d0b8a87d6e83
 
 expr = str()
@@ -10,6 +11,14 @@ calc.config(background='#F8EDE3')
 frame = Frame(calc)
 frame.pack()
 
+
+expr_field = Entry(
+    frame,
+    width=24
+)
+expr_field.grid(columnspan=3, row=0)
+
+
 num_btn = list()
 for i in range(1, 10):
     button = Button(
@@ -17,9 +26,11 @@ for i in range(1, 10):
         text=f"  {i}  ",
         fg='black',
         bg='#D0B8A8',
-        command=None
+        command=None,
+        width=5
     )
-    button.grid(row=(i-1) // 3, column=(i-1) % 3)
+    button.grid(row=1 + (i-1) // 3, column=(i-1) % 3)
     num_btn.append(button)
+
 
 calc.mainloop()
